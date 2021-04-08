0def9cc990e1c2ee9a38c76aa517878041acc763
Saban Karaman <karaman.saban@gmail.com>
2021-04-09 00:01:48 +0300

    second version

---
+++ bid_secret.py
@@ -0,0 +1,21 @@
+from art import logo
+print(logo)
+bid_dic = {}
+def bid(name, price):
+  bid_dic[name] = price
+  
+should_end = True
+while should_end == True:
+  name_bid = input("What's your name ? : \n")
+  bid_price = int(input(f"{name_bid} what's your bid ?  $"))
+  bid(name = name_bid, price = bid_price)
+
+  restart = input("Type 'yes' There is another person. Otherwise type 'no'. \n").lower()
+  if restart == "yes":
+      should_end = True
+  elif restart == "no":
+      should_end = False
+      x = max(bid_dic.values())
+      for i in bid_dic:
+        if x == bid_dic[i]:
+          print(f"Congrat highest bid {i} with ${x}") 
