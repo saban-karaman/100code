5cdb8aa4d16baac5d6a5f2c27509e1fb77d04c4c
Saban Karaman <karaman.saban@gmail.com>
2021-04-18 15:58:09 +0300

    final change keep number

--- keep_number.py
+++ keep_number.py
@@ -2,37 +2,45 @@ import random
 from keep_number_logo import logo
 print(logo)
 difficulty = input("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.\nChoose a difficulty. Type 'easy' or 'hard': ").lower()
-def game():
-  number = random.randint(1,100)
-  make_guess = 1
-  def guess():
-    make_guess = int(input("make a guess: "))
-    if number == make_guess:
-      print("Congrat! you are awesome")
-    elif number < make_guess:
-      print("Too high.")
-    else:
-      print("Too low.")
-  def easy():
-    i = 0
-    while make_guess != number and i != 10:
-        guess()
-        i += 1
-        if make_guess != number:
-          print(f"You have {10 - i} attemp remainig.")
-    if i == 10:
-      print("You Lose :D")
-  def hard():
-    i = 0
-    while make_guess != number and i != 5:
-        guess()
-        i += 1
-        if make_guess != number:
-          print(f"You have {5 - i} attemp remainig.")
-    if i == 5:
-      print("You Lose :D")
-  if difficulty == "easy":
-    easy()
+number = random.randint(1,100)
+make_guess = 1
+def guess():
+  global number
+  global make_guess 
+  make_guess = int(input("make a guess: "))
+  if number == make_guess:
+    print("Congrat! you are awesome")
+  elif number < make_guess:
+    print("Too high.")
   else:
-    hard()  
-game()  
+    print("Too low.")
+def easy():
+  global number
+  global make_guess
+  i = 0
+  while make_guess != number and i != 10:
+      guess()
+      i += 1
+      if make_guess != number:
+        print(f"You have {10 - i} attemp remainig.")
+  if make_guess == number:
+      print("bravo")      
+  elif i == 10:
+    print("You Lose :D")
+def hard():
+  global number
+  global make_guess
+  i = 0
+  while make_guess != number and i != 5:
+      guess()
+      i += 1
+      if make_guess != number:
+        print(f"You have {5 - i} attemp remainig.")
+  if make_guess == number:
+      print("bravo")      
+  elif i == 5:
+    print("You Lose :D")
+if difficulty == "easy":
+  easy()
+else:
+  hard() 
