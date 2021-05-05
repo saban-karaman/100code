8abae7ba3fbe1003bed10a149d430fbff31217a9
Saban Karaman <karaman.saban@gmail.com>
2021-05-05 19:19:51 +0300

    final fizz_buzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,10 +4,10 @@ game = []
 for i in range(1,101):
     game.append(i)
     if i % 3 == 0 and i % 5 == 0:
-        game[i-1] = "fizz buzz"
+        game[i-1] = "FizzBuzz"
     elif i % 3 == 0:
-        game[i -1] ="fizz"
+        game[i -1] ="Fizz"
     elif i % 5 == 0:
-        game[i - 1] = "buzz"
-print(game)
+        game[i - 1] = "Buzz"
+    print(game[i-1])
 
