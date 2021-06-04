f44567006a25f5a2c797d84b58f0534cdbe3fc2a
Saban Karaman <karaman.saban@gmail.com>
2021-06-05 00:10:31 +0300

    final

--- Birthday_Wisher/birthday/main.py
+++ Birthday_Wisher/birthday/main.py
@@ -1,32 +1,38 @@
-##################### Extra Hard Starting Project ######################
+#To run and test the code you need to update 4 places:
+# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
+# 2. Go to your email provider and make it allow less secure apps.
+# 3. Update the SMTP ADDRESS to match your email provider.
+# 4. Update birthdays.csv to contain today's month and day.
+# See the solution video in the 100 Days of Python Course for explainations.
 
-# 1. Update the birthdays.csv
 
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
+from datetime import datetime
 import pandas
-import datetime as dt
+import random
+import smtplib
 
-data = [
-    ["ivan", "aiv@gmail.com", "1983", "15", "7"]
-    ]
+MY_EMAIL = "ex100code@gmail.com"
+MY_PASSWORD = "Ankara06."
 
-dataframe = pandas.DataFrame(data)
-dataframe.to_csv("Birthday_Wisher/birthday/birthdays.csv", index=False, mode="a", header=False)
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-now = dt.datetime.now()
-day = now.day
-month = now.month
+data = pandas.read_csv("Birthday_Wisher/birthday/birthdays.csv")
 
-with open("Birthday_Wisher/birthday/birthdays.csv", "r", encoding="utf-8") as f:
-    birtdays = f.readlines()
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"Birthday_Wisher/birthday/letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-
-full_data = pandas.read_csv("Birthday_Wisher/birthday/birthdays.csv")
-first= full_data[["month", "day"]]
-print(first.iloc[month])
-
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
